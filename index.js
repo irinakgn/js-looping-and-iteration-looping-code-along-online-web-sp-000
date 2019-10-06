4103c191346404df161765b0207702167d6179d0
Irina Kagan <irinagamb@gmail.com>
2019-10-06 13:41:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+
+const writeCards=(["Ada", "Brendan", "Ali"], "birthday");
+function writeCards(writeCards){
+  for (let i = 0; i < writeCards.length; i++){
+  console.log(`Thank you,${name}, for the wonderful birthda gift!`);
+}
+}
