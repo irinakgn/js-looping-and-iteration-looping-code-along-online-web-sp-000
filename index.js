7f6ac253920f3e84ee9ac0c9b1db3eafe6bf0956
Irina Kagan <irinagamb@gmail.com>
2019-10-06 13:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 const writeCards=(["Ada", "Brendan", "Ali"], "birthday");
 function writeCards(writeCards){
   for (let i = 0; i < writeCards.length; i++){
-  console.log(`Thank you,${name}, for the wonderful birthda gift!`);
+  console.log(`Thank you,${wrietCards}, for the wonderful birthda gift!`);
 }
 }
